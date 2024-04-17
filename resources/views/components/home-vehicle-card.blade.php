0e827c2185b188597081027b00d25f04265d01db
MKashanButt <kashanbutt566@gmail.com>
2024-04-18 02:26:24 +0500

    added symbol

--- resources/views/components/home-vehicle-card.blade.php
+++ resources/views/components/home-vehicle-card.blade.php
@@ -1,6 +1,6 @@
 <div class="item">
     <a href="/vehicle-info/{{ $id }}">
-        <img src="{{ asset('storage/images' . $img) }}" alt="stock-image">
+        <img src="{{ asset('storage/images/' . $img) }}" alt="stock-image">
         <h4>{{ $make }} {{ $model }}</h4>
         <p>{{ $currency }} <span>{{ $fob }}</span></p>
     </a>
