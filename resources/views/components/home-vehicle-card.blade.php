99e7d14b6809cdaa7dd05a9b48425923be5cd85a
MKashanButt <kashanbutt566@gmail.com>
2024-04-18 02:28:26 +0500

    updated link

--- resources/views/components/home-vehicle-card.blade.php
+++ resources/views/components/home-vehicle-card.blade.php
@@ -1,6 +1,6 @@
 <div class="item">
     <a href="/vehicle-info/{{ $id }}">
-        <img src="{{ Storage::url('storage/images/' . $img) }}" alt="stock-image">
+        <img src="{{ Storage::url('images/' . $img) }}" alt="stock-image">
         <h4>{{ $make }} {{ $model }}</h4>
         <p>{{ $currency }} <span>{{ $fob }}</span></p>
     </a>
