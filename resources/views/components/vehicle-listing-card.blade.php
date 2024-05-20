d1797c3621eeb249b42ac1db899d897bab1ea5e5
MKashanButt <kashanbutt566@gmail.com>
2024-05-21 02:05:18 +0500

    removed format

--- resources/views/components/vehicle-listing-card.blade.php
+++ resources/views/components/vehicle-listing-card.blade.php
@@ -5,7 +5,7 @@
             <h3>{{ strtoupper($year) }} {{ strtoupper($make) }} {{ strtoupper($model) }}</h3>
             <div class="text">
                 <p>Year: <span>{{ $year }}</span></p>
-                <p>Mileage: <span>{{ number_format($mileage) }}</span></p>
+                <p>Mileage: <span>{{ $mileage }}</span></p>
                 <p>Engine: <span>{{ $engine }}</span></p>
                 <p>Doors: <span>{{ $doors }}</span></p>
                 <p>Transmission: <span>{{ $transmission }}</span></p>
