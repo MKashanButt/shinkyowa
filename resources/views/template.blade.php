39ee5a14c3b5932dd5b84e5578e65e2103b7be51
MKashanButt <kashanbutt566@gmail.com>
2024-05-22 01:35:26 +0500

    updated slider js

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -96,16 +96,10 @@
             loop: true,
             spaceBetween: 10,
             slidesPerView: 4,
-            freeMode: true,
             watchSlidesProgress: true,
         });
         var swiper2 = new Swiper(".mySwiper2", {
             loop: true,
-            spaceBetween: 10,
-            navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev",
-            },
             thumbs: {
                 swiper: swiper,
             },
