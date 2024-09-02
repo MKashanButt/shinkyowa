476e8945039462a9111e02fe255967c589e04785
MKashanButt <kashanbutt566@gmail.com>
2024-09-02 22:33:24 +0500

    - removed testimonial commit and updated url on gif - fixed sidebar item alignment issue on mobile

--- resources/views/components/second-sidebar.blade.php
+++ resources/views/components/second-sidebar.blade.php
@@ -25,35 +25,10 @@
     <div class="offers">
         <a href="/stock"><img src="{{ env('IMAGE_API_URL') . 'images/offers/offer-one.gif' }}" alt=""></a>
         <a href="/stock"><img src="{{ env('IMAGE_API_URL') . 'images/offers/offer-two.gif' }}" alt=""></a>
-        <a href="/stock"><img src="{{ env('IMAGE_API_URL') . 'images/offers/offer-three.gif' }}" alt=""></a>
+        <a href="/filter?yearfrom=2024"><img src="{{ env('IMAGE_API_URL') . 'images/offers/offer-three.gif' }}"
+                alt=""></a>
     </div>
     <hr>
-    {{-- <div class="testimonials">
-        <h2>Testimonials</h2>
-        <div class="stage">
-            <div class="item">
-                <img src="https://www.carjunction.com/testi_images/558/Image%2001.png" alt="">
-                <p>Thank you very much I received my vehicle with good condition of everything. I am very happy to
-                    purchase
-                    a
-                    vehicle from Car Junction....</p>
-            </div>
-            <div class="item">
-                <img src="https://www.carjunction.com/testi_images/558/Image%2001.png" alt="">
-                <p>Thank you very much I received my vehicle with good condition of everything. I am very happy to
-                    purchase
-                    a
-                    vehicle from Car Junction....</p>
-            </div>
-            <div class="item">
-                <img src="https://www.carjunction.com/testi_images/558/Image%2001.png" alt="">
-                <p>Thank you very much I received my vehicle with good condition of everything. I am very happy to
-                    purchase
-                    a
-                    vehicle from Car Junction....</p>
-            </div>
-        </div>
-    </div> --}}
 </aside>
 <script>
     var vehicleSwiper = new Swiper(".vehicleSwiper", {
