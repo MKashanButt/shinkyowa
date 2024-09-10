2d21527322a6be6085c7fc08278f22f2939026d5
MKashanButt <kashanbutt566@gmail.com>
2024-09-10 21:46:24 +0500

    updated map route function

--- resources/views/locationtest.blade.php
+++ resources/views/locationtest.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Leaflet Location Map</title>
+    <title>Location Map</title>
     <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
     <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
     <style>
@@ -20,17 +20,17 @@
     <div id="map"></div>
 
     <script>
-        // Initialize the map
-        var map = L.map('map').setView([{{ $location['latitude'] }}, {{ $location['longitude'] }}], 13);
+        // Initialize the map using the latitude and longitude from the backend
+        var map = L.map('map').setView([{{ $latitude }}, {{ $longitude }}], 13);
 
         // Load the map tiles from OpenStreetMap
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
-        // Add a marker at the specified location
-        L.marker([{{ $location['latitude'] }}, {{ $location['longitude'] }}]).addTo(map)
-            .bindPopup('Location: {{ $location['cityName'] }}, {{ $location['countryName'] }}')
+        // Add a marker with a popup
+        L.marker([{{ $latitude }}, {{ $longitude }}]).addTo(map)
+            .bindPopup('Location: {{ $city }}, {{ $country }}')
             .openPopup();
     </script>
 </body>
